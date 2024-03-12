b6a17be1f6c9a4577a80ab110ec8abd457aa94d2
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-03-12 21:12:57 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import {useState,useEffect} from 'react';
 
 function App() {
-  let [value,setvalue] = useState(0);
+  const [value,setvalue] = useState(0);
+  let [val1, setval1] = useState(0);
   useEffect(()=>{
    console.log(value, "value");
-  })
+  },[value])
   return (
     <div className="App">
       <h1>count value {value}</h1>
-      <button onClick={setvalue(value+1)}>Add</button>
+      <button onClick={()=>{setvalue(value+1)}}>Add</button>
+      <button onClick={()=>{setval1(val1+1)}}>Add</button>
     </div>
   );
 }
