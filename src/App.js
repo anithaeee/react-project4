bf8bb3974169b47417ad7bbe3bf4814f7fc085d5
Anitha Reddy Pandem <anithaeee224@gmail.com>
2024-03-15 08:54:10 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import {useState,useEffect} from 'react';
-
+import Props from './Props';
 function App() {
   const [value,setvalue] = useState(0);
   let [val1, setval1] = useState(0);
+  const [counterval, setcounterval] = useState(0);
   useEffect(()=>{
    console.log(value, "value");
+   Updateval();
   },[value])
+  const Updateval = () =>{
+    setcounterval(counterval+1);
+  }
   return (
     <div className="App">
-      <h1>count value {value}</h1>
+      <Props count={counterval}/>
       <button onClick={()=>{setvalue(value+1)}}>Add</button>
       <button onClick={()=>{setval1(val1+1)}}>Add</button>
     </div>
---
+++ src/Props.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useState, useEffect} from 'react'
+const Props = (props) => {
+  return (
+    <div>
+      <h1>{props.count}</h1>
+    </div>
+  )
+}
+
+export default Props;
